fb7d1f2101b70c35cde9336660d671bb4e26379e
Brian Miranda <brianmiranda@outlook.com>
2018-12-07 18:08:56 -0600

    Remove intro bg and 4th service column. First functioning overlay menu. Almost out of pre-alpha

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,10 +10,4 @@ $(document).ready(function() {
     else sticky.removeClass('fixed');
   });
 
-  $(".menu-link").click(function(event) {
-    event.preventDefault();
-    $(".menu-overlay").toggleClass("open");
-    $(".menu").toggleClass("open");
-  });
-
 });
