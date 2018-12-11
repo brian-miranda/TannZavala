d865d01f2ce029df664396fab20cf157750ec286
Brian Miranda <brianmiranda@outlook.com>
2018-12-11 17:24:54 -0600

    Finally fix overlay menu and the white menu logo. Alpha build

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,13 +1,4 @@
 $(document).ready(function() {
-  // header animation
-  var stickyOffset = $(".sticky").offset().top;
-
-  $(window).scroll(function(){
-    var sticky = $(".sticky"),
-        scroll = $(window).scrollTop();
-
-    if (scroll >= stickyOffset) sticky.addClass("fixed");
-    else sticky.removeClass("fixed");
-  });
+  
 
 });
