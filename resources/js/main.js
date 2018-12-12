b2ed50b3eaa920ebcb6128705e09635935531b46
Brian Miranda <brianmiranda@outlook.com>
2018-12-12 15:18:19 -0600

    Add project page and fix bottom margin. Mid-day commit

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
-  
+  // header animation
+  var stickyOffset = $(".sticky").offset().top;
+
+  $(window).scroll(function(){
+    var sticky = $(".sticky"),
+        scroll = $(window).scrollTop();
+
+    if (scroll >= stickyOffset) sticky.addClass("fixed");
+    else sticky.removeClass("fixed");
+  });
+
+  /* Open */
+  function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
 
 });
